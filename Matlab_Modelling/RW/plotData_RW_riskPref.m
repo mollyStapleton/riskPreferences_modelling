cd9885429ef7221d68aab9300718c576a8210d2a
mollyStapleton <molly.stapleton@bristol.ac.uk>
2023-09-28 21:42:51 +0100

    Fits vs True data for accuracy ~ P(High)

--- Matlab_Modelling/RW/plotData_RW_riskPref.m
+++ Matlab_Modelling/RW/plotData_RW_riskPref.m
@@ -1,4 +1,4 @@
-function [meanTrue, meanFit, binnedTrue, binnedFit] = plotFit_RW_riskPref(dataIn, paramFit, dist)
+function [meanTrue, meanFit, binnedTrue, binnedFit, accTrue, accFit] = plotFit_RW_riskPref(dataIn, paramFit, dist)
 
     if strcmp(dist, 'Gaussian')
         col2plot = {[0.83 0.71 0.98], [0.62 0.35 0.99]};
@@ -26,25 +26,93 @@ function [meanTrue, meanFit, binnedTrue, binnedFit] = plotFit_RW_riskPref(dataIn
 
     data2use.choiceProb = choiceProb';
     data2use.choiceL    = choice_dir';
+
+    % overall p(risky)
+    stimCombo = {[21 12], [43 34]};
+
+    for icnd = 1:2
+
+        tmp = [];
+
+        for icmb = 1:2
+
+            stimIdx = [];
+            stimIdx = find(data2use.stimComboIdx == stimCombo{icnd}(icmb));
+            if icmb == 1 % risky stimulus is on the LEFT
+                tmp     = [tmp; data2use.choiceProb(stimIdx)];
+            else 
+                tmp     = [tmp; 1 - data2use.choiceProb(stimIdx)];
+            end
+        
+        end
+
+        meanFit(icnd) = mean(tmp);
+    end
+
+    
+
     % trial binned risk preference calculations
     bin = [1:24:120];
     binSize = 24;
-    for ibin = 1: length(bin)
+    binnedFit = [];
 
-        trIdx = [];
-        trIdx = find(ismember(data2use.trialNum, (bin(ibin): (bin(ibin) + binSize -1)))==1);
-
-        binned_cp{1}(ibin) = sum(data2use.choiceProb(data2use.choiceType(trIdx) == -1))./sum(data2use.cnd_idx(trIdx) == 2);
-        binned_cp{2}(ibin) = sum(data2use.choiceProb(data2use.choiceType(trIdx) == 1))./sum(data2use.cnd_idx(trIdx) == 3);
+    for icnd = 1:2
+        
+        for ibin = 1: length(bin)
     
-%         binned_cp{1}(ibin) = nanmean(data2use.choiceProb(data2use.choiceType(trIdx) == -1));
-%         binned_cp{2}(ibin) = nanmean(data2use.choiceProb(data2use.choiceType(trIdx) == 1));
-%     
+            trIdx = [];
+            trIdx = find(ismember(data2use.trialNum, (bin(ibin): (bin(ibin) + binSize -1)))==1);
+            tmp   = [];
+
+            for icmb = 1:2
+
+                stimIdx = [];
+                stimIdx = trIdx(data2use.stimComboIdx(trIdx) == stimCombo{icnd}(icmb));
+                if icmb == 1 % risky stimulus is on the LEFT
+                    tmp     = [tmp; data2use.choiceProb(stimIdx)];
+                else
+                    tmp     = [tmp; 1 - data2use.choiceProb(stimIdx)];
+                end
+            end
+
+            binnedFit{icnd}(ibin) = mean(tmp);
+            
+        end
     end
 
-    meanFit(1, :) = nanmean(data2use.choiceProb(data2use.choiceType == -1));
-    meanFit(2, :) = nanmean(data2use.choiceProb(data2use.choiceType == 1));
+    % return p(High) for all 4 of the unmatched conditions 
+    % 3 and 4 are the high stimuli
+    stimHighIdx = {[41 14], [31 13], [32 23], [42 24]};
+    for istim = 1:4
+        tmp = [];
 
-    binnedFit =binned_cp;
+        for ibin = 1: length(bin)
     
-end 
+            trIdx = [];
+            trIdx = find(ismember(data2use.trialNum, (bin(ibin): (bin(ibin) + binSize -1)))==1);
+            tmp   = [];
+            fullIdx = [];
+            
+                for icmb = 1:2
+                    stimIdx = [];
+                    stimIdx = trIdx(data2use.stimComboIdx(trIdx) == stimHighIdx{istim}(icmb));
+                    if icmb == 1 
+                        tmp = [tmp; data2use.choiceProb(stimIdx)];
+                    else 
+                        tmp = [tmp; 1- data2use.choiceProb(stimIdx)];
+                    end
+             
+                    fullIdx = [fullIdx; stimIdx];
+                    
+                end
+
+        accFit(istim, ibin) = mean(tmp);
+
+        accTrue(istim, ibin) =  sum(data2use.choice_high(fullIdx) == 1)./length(fullIdx);
+
+    end
+
+    
+    end 
+
+end
--- Matlab_Modelling/plotData_riskPref.m
+++ Matlab_Modelling/plotData_riskPref.m
@@ -1,7 +1,7 @@
-function [meanTrue, meanFit, binnedTrue, binnedFit] = plotFit_riskPref(dataIn, paramFit, model, dist)
+function [meanTrue, meanFit, binnedTrue, binnedFit, accTrue, accFit] = plotFit_riskPref(dataIn, paramFit, model, dist)
 
 if strcmpi(model, 'RW')
-   [meanTrue, meanFit, binnedTrue, binnedFit] = plotData_RW_riskPref(dataIn, paramFit, dist);
+   [meanTrue, meanFit, binnedTrue, binnedFit, accTrue, accFit] = plotData_RW_riskPref(dataIn, paramFit, dist);
 end
 
 end
